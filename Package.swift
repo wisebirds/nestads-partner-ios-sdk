30c95e1ca8cde690e6b1e9aacf8110a9b2dedb7e
Bitbucket Pipeline <ci@wisebirds.com>
2026-07-09 15:26:04 +0900

    release v1.1.0

--- Package.swift
+++ Package.swift
@@ -12,21 +12,23 @@ let package = Package(
     )
   ],
   dependencies: [
-    .package(url: "https://github.com/wisebirds/nestads-ios-sdk", from: "2.14.3"),
-    .package(url: "https://github.com/covigroup/COVI-iOS-SDK.git", from: "1.2.1")
+    .package(url: "https://github.com/wisebirds/nestads-ios-sdk", from: "2.15.0"),
+    .package(url: "https://github.com/covigroup/COVI-iOS-SDK.git", from: "1.2.1"),
+    .package(url: "https://github.com/moloco/moloco-sdk-ios-spm.git", from: "4.8.0")
   ],
   targets: [
     .binaryTarget(
       name: "NestAdsPartnerSDK",
-      url: "https://github.com/wisebirds/nestads-partner-ios-sdk/releases/download/1.0.2/NestAdsPartnerSDK.xcframework.zip",
-      checksum: "35aec9e4d5bb22b015d7476b19b764016ea185a72bb486f6fc98a731f7e044be"
+      url: "https://github.com/wisebirds/nestads-partner-ios-sdk/releases/download/1.1.0/NestAdsPartnerSDK.xcframework.zip",
+      checksum: "a36a7b42fba2f960ff51b5572d6d85ac7b23a59f55eb0e58a65738112dbd3929"
     ),
     .target(
       name: "NestAdsPartnerSDKWrapper",
       dependencies: [
         "NestAdsPartnerSDK",
         .product(name: "NestAdsSDK", package: "nestads-ios-sdk"),
-        .product(name: "COVI-iOS-SDK", package: "COVI-iOS-SDK")
+        .product(name: "COVI-iOS-SDK", package: "COVI-iOS-SDK"),
+        .product(name: "MolocoSDK", package: "moloco-sdk-ios-spm")
       ],
       path: "Sources/Wrapper"
     )
